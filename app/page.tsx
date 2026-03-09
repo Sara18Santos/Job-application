85f5a230524d8bec93ae51c4ebb247b9f82875e1
Sara Ataíde Santos <249837677+sara-santos_valtech@users.noreply.github.com>
2026-03-09 11:33:33 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <main className="flex-1">
+        <section className="container mx-auto px-4 py-32">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold"> A better way to track your job applications</h1>
+            <p className="text-muted-foreground mb-10 text-xl">
+              Capture, organize, and manage you job search in one place.
+            </p>
+            <div>
+              <button>Start for free</button>
+              <p>Free forever. No credit card required.</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
