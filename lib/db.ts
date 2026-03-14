0c6525ea80a02a9e2d2bec1054ae265658b18484
Sara18Santos <sataide.santos@gmail.com>
2026-03-14 19:34:30 +0000

    feat: update MongoDB connection handling and add better-auth dependency

--- lib/db.ts
+++ lib/db.ts
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const MONGO_URL = process.env.MONGO_URL;
 
-if (!MONGO_URL) {
-  throw new Error("MONGO_URL environment variable is not defined");
-}
-
 interface MongooseCache {
   conn: typeof mongoose | null;
   promise: Promise<typeof import("mongoose")> | null;
@@ -20,3 +16,35 @@ let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 if (!global.mongoose) {
   global.mongoose = cached;
 }
+
+async function connectDB() {
+  if (!MONGO_URL) {
+    throw new Error(
+      "Please define the MONGO_URL environment variable indice .env"
+    );
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
